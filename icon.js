73939c76b9fcf5e1ee9a1a9b53647aa891e4db7d
animeplay306-dev <animeplay306@gmail.com>
2026-04-04 22:54:28 +0300

    Update project files

---
+++ icon.js
@@ -0,0 +1,16 @@
+{
+  "icon": {
+    "name": "omar.png",
+    "path": "/data/data/com.termux/files/home/القران/icon/omar.png",
+    "original_size": "750x750",
+    "target_size": "512x512",
+    "format": "png",
+    "description": "أيقونة التطبيق الخاصة بتطبيق القرآن",
+    "created_at": "2026-04-04",
+    "author": {
+      "name": "Omar",
+      "website": "https://animeplay306-dev.github.io/quran-website/"
+    },
+    "usage": "ستُستخدم كأيقونة رئيسية عند إنشاء APK بواسطة Bubblewrap أو أي أداة PWA to APK"
+  }
+}
